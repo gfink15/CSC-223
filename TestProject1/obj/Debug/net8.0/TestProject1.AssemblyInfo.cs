f51433dde3649e5ffac79ad59e6d68a5e4e9d1f7
gfink15 <graham.fink14@gmail.com>
2026-01-22 10:10:31 -0500

    s

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500780f99f56ab7ed2cb4bb78467cbfb22f75ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d595996400d5ee91988cbed4277d4de09e8b5c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

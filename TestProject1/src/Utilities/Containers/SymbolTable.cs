74ef50266940b3b35751001d3a57e120c2675b55
gfink15 <graham.fink14@gmail.com>
2026-01-28 16:06:05 -0500

    Lab progress

---
+++ TestProject1/src/Utilities/Containers/SymbolTable.cs
@@ -0,0 +1,164 @@
+/**
+ * Doubly Linked List Class: Provide functionality for
+ * a doubly linked list structure
+ * Claude AI was only used for writing XML and inline comments.
+ * 
+ * @author Graham Fink, Mridul Agrawal
+ * @date   1/28/2026
+ */
+
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Utilities.Containers;
+
+
+public class SymbolTable<TKey, TValue> : IDictionary<TKey, TValue>
+{
+    private DLL<TKey> _KeyDLL = new DLL<TKey>();
+    private DLL<TValue> _ValueDLL = new DLL<TValue>();
+    private SymbolTable<TKey, TValue> _parent;
+    private int _sz = 0;
+
+    public SymbolTable<TKey, TValue> Parent
+    {
+        get { return _parent; }
+    }
+
+    public ICollection<TKey> Keys
+    {
+        get { return _KeyDLL; }
+    }
+
+    public ICollection<TValue> Values
+    {
+        get { return _ValueDLL; }
+    }
+
+    public int Count
+    {
+        get { return _sz; }
+    }
+
+    public bool IsReadOnly
+    {
+        get { return false; }
+    }
+
+    public TValue this[TKey key]
+    { 
+        get 
+        {
+            if (TryGetValueLocal(key, out TValue value)) return value;
+            else
+            {
+                throw new KeyNotFoundException("Key does not exist");
+            }
+        }
+        set
+        {
+            if (ContainsKeyLocal(key))
+            {
+                int keyIndex = _KeyDLL.IndexOf(key);
+                _ValueDLL[keyIndex] = value;
+            }
+            else
+            {
+                Add(new KeyValuePair<TKey, TValue>(key, value));
+            }
+        }
+    }
+
+    public SymbolTable(SymbolTable<TKey, TValue> parent)
+    {
+        _parent = parent;
+    }
+
+    public bool ContainsKeyLocal(TKey key)
+    {
+        if (key == null) throw new ArgumentNullException("Key is null");
+        return _KeyDLL.Contains(key);
+    }
+
+    public bool TryGetValueLocal(TKey key, out TValue value)
+    {
+        if (ContainsKeyLocal(key))
+        {
+            int keyIndex = _KeyDLL.IndexOf(key);
+            value = _ValueDLL[keyIndex];
+            return true;
+        }
+        else
+        {
+            value = default;
+            return false;
+        }
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        if (ContainsKeyLocal(key)) throw new ArgumentException("Key already exists");
+        else
+        {
+            _KeyDLL.Add(key);
+            _ValueDLL.Add(value);
+            _sz++;
+        }
+    }
+
+    public bool ContainsKey(TKey key)
+    {
+        return ContainsKeyLocal(key);
+    }
+
+    public bool Remove(TKey key) // not finished
+    {
+        if (!ContainsKeyLocal(key)) throw new KeyNotFoundException("Key not found");
+        int indexToRemove = _KeyDLL.IndexOf(key);
+        _KeyDLL.RemoveAt(indexToRemove);
+        _ValueDLL.RemoveAt(indexToRemove);
+        return false;
+    }
+
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(KeyValuePair<TKey, TValue> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}

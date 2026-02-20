5db0599b38802dba2bd794e0be3eb88a9b0a5fbf
Mridul Agrawal <agramr2@furman.edu>
2026-02-19 21:29:48 -0500

    Included comments, docstrings, and integer division and exponentiation

--- TestProject1/src/Tokenizer/Token_Mridul.cs
+++ TestProject1/src/Tokenizer/Token_Mridul.cs
@@ -6,6 +6,9 @@ using Xunit.Sdk;
 
 namespace Tokenizer_Mridul;
 
+/// <summary>
+/// Defines the types of tokens recognized by the tokenizer.
+/// </summary>
 public enum TokenType
 {
     VARIABLE,
@@ -19,6 +22,10 @@ public enum TokenType
     LEFT_CURLY,
     RIGHT_CURLY
 }
+
+/// <summary>
+/// Constants for recognized token values and operators.
+/// </summary>
 public static class TokenConstants
 {
     public const string LEFT_PAREN = "(";
@@ -28,38 +35,64 @@ public static class TokenConstants
     public const string MINUS = "-";
     public const string MULTIPLY = "*";
     public const string DIVIDE = "/";
+    public const string INTEGERDIVIDE = "//";
+    public const string EXPONENTIATE = "**";
     public const string ASSIGNMENT = ":=";
     public const string DECIMAL_POINT = ".";
     public const string LEFT_CURLY = "{";
     public const string RIGHT_CURLY = "}";
 }
 
+/// <summary>
+/// Represents a single token with a type and value.
+/// </summary>
 public class Token
 {
     private readonly TokenType _type;
     private readonly string _value;
 
+    /// <summary>
+    /// Gets the type of this token.
+    /// </summary>
     public TokenType Type
     {
         get { return _type;}
     }
 
+    /// <summary>
+    /// Gets the string value of this token.
+    /// </summary>
     public string Value
     {
         get {return _value;}
     }
 
+    /// <summary>
+    /// Constructs a new token with the specified value and type.
+    /// </summary>
+    /// <param name="Value">The string value of the token.</param>
+    /// <param name="Type">The type of the token.</param>
     public Token(string Value, TokenType Type)
     {
         _value = Value;
         _type = Type;
     }
 
+    /// <summary>
+    /// Returns a string representation of this token.
+    /// </summary>
+    /// <returns>String in format "Value : Type".</returns>
     public override string ToString()
     {
         return Value + " : " + Type.ToString();
     }
 
+    /// <summary>
+    /// Compares this token to another object for equality based on value and type.
+    /// </summary>
+    /// <param name="obj">The object to compare with.</param>
+    /// <returns>True if both tokens have the same value and type.</returns>
+    /// <exception cref="ArgumentException">Thrown when obj is not a Token.</exception>
     public override bool Equals(Object? obj)
     {
         if (obj is Token)
--- TestProject1/src/Tokenizer/TokenizerImpl_Mridul.cs
+++ TestProject1/src/Tokenizer/TokenizerImpl_Mridul.cs
@@ -1,20 +1,37 @@
+/**
+ * Doubly Linked List Class: Provide functionality for
+ * a doubly linked list structure
+ * Claude AI was only used for writing XML and inline comments.
+ * 
+ * @author Graham Fink, Mridul Agrawal
+ * @date   1/21/2026
+ */
+
 using System;
 using System.Data.Common;
 using System.Reflection.Metadata;
 
-
 namespace Tokenizer_Mridul;
 
+/// <summary>
+/// Provides tokenization functionality for parsing source code into tokens.
+/// </summary>
 public static class TokenizerImpl
 {
+    /// <summary>
+    /// Tokenizes the given source code into a list of tokens.
+    /// </summary>
+    /// <param name="source_code">The source code string to tokenize.</param>
+    /// <returns>A list of tokens parsed from the source code.</returns>
     public static List<Token> Tokenize(string source_code)
     {
         List<Token> Tokens = new List<Token>();
-        string curr = "";
-        string currType = "";
-        string extendedSourceCode = source_code + " ";
+        string curr = "";  // Current token being built
+        string currType = "";  // Type of current token being built
+        string extendedSourceCode = source_code + " ";  // Add space to process last token
         foreach (char c in extendedSourceCode)
         {
+            // Handle completion of number tokens
             if (currType == "number")
             {
                 if (!char.IsDigit(c) && c != '.')
@@ -25,6 +42,7 @@ public static class TokenizerImpl
                 }
                 else curr += c;
             }
+            // Handle completion of string tokens (keywords or variables)
             else if (currType == "string")
             {
                 if (!char.IsLetter(c))
@@ -35,6 +53,7 @@ public static class TokenizerImpl
                 }
                 else curr += c;
             }
+            // Handle completion of assignment tokens
             else if (currType == "assignment")
             {
                 if (c != '=')
@@ -48,20 +67,29 @@ public static class TokenizerImpl
                     curr += c;
                 }
             }
+            // Handle completion of parentheses and curly braces
             else if (currType == "parens")
             {
                 Tokens.Add(HandleParensAndCurly(curr));
                 curr = "";
                 currType = "";
             }
+            // Handle completion of operator tokens
             else if (currType == "operator")
             {
-                Tokens.Add(HandleOperator(curr));
-                curr = "";
-                currType = "";
+                if (c != '/' && c != '*')
+                {
+                    Tokens.Add(HandleOperator(curr));
+                    curr = "";
+                    currType = "";
+                }
+                else
+                {
+                    curr += c;
+                }
             }
 
-            // Now, process the current character if we just finished building a token, that it, the last character in the token was the c-1 character.
+            // Start building a new token based on current character
             if (currType == "")
             {   
                 if (c == '(' || c == ')' || c == '{' || c == '}') (curr, currType) = (c.ToString(), "parens");
@@ -77,23 +105,44 @@ public static class TokenizerImpl
         return Tokens;
     }
 
+    /// <summary>
+    /// Creates an assignment token.
+    /// </summary>
+    /// <param name="curr">The current token string.</param>
+    /// <returns>An assignment token.</returns>
     public static Token HandleAssignment(string curr)
     {
         return new Token(TokenConstants.ASSIGNMENT, TokenType.ASSIGNMENT);
     }
 
+    /// <summary>
+    /// Creates an integer or double token based on the presence of a decimal point.
+    /// </summary>
+    /// <param name="curr">The current token string.</param>
+    /// <returns>A token of type INTEGER or DOUBLE.</returns>
     public static Token HandleIntegerOrDouble(string curr)
     {
         if (curr.Contains('.')) return new Token(curr, TokenType.DOUBLE);
         else return new Token(curr, TokenType.INTEGER);
     }
 
+    /// <summary>
+    /// Creates a token for keywords or variables.
+    /// </summary>
+    /// <param name="curr">The current token string.</param>
+    /// <returns>A RETURN token if the string is "return", otherwise a VARIABLE token.</returns>
     public static Token HandleString(string curr)
     {
         if (curr == "return") return new Token(TokenConstants.RETURN, TokenType.RETURN);
         else return new Token(curr, TokenType.VARIABLE);
     }
 
+    /// <summary>
+    /// Creates a token for parentheses or curly braces.
+    /// </summary>
+    /// <param name="curr">The current token string.</param>
+    /// <returns>The appropriate parenthesis or curly brace token.</returns>
+    /// <exception cref="Exception">Thrown when the character is not a valid parenthesis or curly brace.</exception>
     public static Token HandleParensAndCurly(string curr)
     {
         if (curr == "(") return new Token(TokenConstants.LEFT_PAREN, TokenType.LEFT_PAREN);
@@ -103,12 +152,20 @@ public static class TokenizerImpl
         else throw new Exception("Invalid character: " + curr);
     }
 
+    /// <summary>
+    /// Creates a token for arithmetic operators.
+    /// </summary>
+    /// <param name="curr">The current token string.</param>
+    /// <returns>An operator token.</returns>
+    /// <exception cref="Exception">Thrown when the character is not a valid operator.</exception>
     public static Token HandleOperator(string curr)
     {
         if (curr == "+") return new Token(TokenConstants.PLUS, TokenType.OPERATOR);
         else if (curr == "-") return new Token(TokenConstants.MINUS, TokenType.OPERATOR);
         else if (curr == "*") return new Token(TokenConstants.MULTIPLY, TokenType.OPERATOR);
         else if (curr == "/") return new Token(TokenConstants.DIVIDE, TokenType.OPERATOR);
+        else if (curr == "//") return new Token(TokenConstants.INTEGERDIVIDE, TokenType.OPERATOR);
+        else if (curr == "**") return new Token(TokenConstants.EXPONENTIATE, TokenType.OPERATOR);
         else throw new Exception("Invalid operator: " + curr);
     }
 }

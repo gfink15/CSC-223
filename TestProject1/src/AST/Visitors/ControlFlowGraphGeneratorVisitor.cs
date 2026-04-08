17b3ab63acfda74408d400fe9964dd953adb7800
Mridul Agrawal <agramr2@furman.edu>
2026-04-08 15:51:29 -0400

    Implemented inetrface

--- TestProject1/src/AST/Visitors/ControlFlowGraphGeneratorVisitor.cs
+++ TestProject1/src/AST/Visitors/ControlFlowGraphGeneratorVisitor.cs
@@ -4,59 +4,63 @@ using System.Runtime.Serialization;
 using System.Text;
 using AST;
 using Utilities.Containers;
+using Optimizer;
 
 namespace AST;
 
 public class ControlFlowGraphGeneratorVisitor : IVisitor<Statement, Statement>
 {
+
+    public CFG _cfg = new CFG();
     public Statement Visit(PlusNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(MinusNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(TimesNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(FloatDivNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(IntDivNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(ModulusNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(ExponentiationNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(LiteralNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(VariableNode node, Statement param)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public Statement Visit(AssignmentStmt node, Statement param)
     {
-        throw new NotImplementedException();
+        // Add Vertex
+        // Add this node to the previous as a directed edge
     }
 
     public Statement Visit(ReturnStmt node, Statement param)

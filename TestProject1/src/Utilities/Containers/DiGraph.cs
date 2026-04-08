4ac1fe316be4e14a8ab5a7e4e65001121536b2a6
Mridul Agrawal <agramr2@furman.edu>
2026-04-08 14:26:10 -0400

    Some of the DiGraph

--- TestProject1/src/Utilities/Containers/DiGraph.cs
+++ TestProject1/src/Utilities/Containers/DiGraph.cs
@@ -9,5 +9,27 @@ namespace Utilities.Containers;
 
 public class DiGraph<T> where T : notnull
 {
-    protected Dictionary<T, DLL<T>> _adjacencyList;
+    protected Dictionary<T, DLL<T>> _adjacencyList = new Dictionary<T, DLL<T>>();
+
+    public DiGraph()
+    {
+    }
+
+    public bool AddVertex(T vertex)
+    {
+        return _adjacencyList.TryAdd(vertex, new DLL<T>());
+    }
+
+    public bool AddEdge(T source, T destination)
+    {
+        if (!_adjacencyList.ContainsKey(source) || !_adjacencyList.ContainsKey(destination))
+        {
+            throw new ArgumentException("Both the Source and the Destination must exist.");
+        }
+        else
+        {
+            _adjacencyList[source].Add(destination);
+            return true;
+        }
+    }
 }

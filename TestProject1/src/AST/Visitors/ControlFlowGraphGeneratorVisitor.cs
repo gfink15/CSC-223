5e5c6492209da4586497af8121a495834b8d76c9
Mridul Agrawal <agramr2@furman.edu>
2026-04-09 22:39:20 -0400

    Update ControlFlowGraphGeneratorVisitor.cs

--- TestProject1/src/AST/Visitors/ControlFlowGraphGeneratorVisitor.cs
+++ TestProject1/src/AST/Visitors/ControlFlowGraphGeneratorVisitor.cs
@@ -8,71 +8,77 @@ using Optimizer;
 
 namespace AST;
 
-public class ControlFlowGraphGeneratorVisitor : IVisitor<Statement, Statement?>
+public class ControlFlowGraphGeneratorVisitor : IVisitor<Statement?, Statement?>
 {
 
     public CFG _cfg = new CFG();
-    private Statement last;
-    public Statement? Visit(PlusNode node, Statement param)
+    private Statement? last;
+    public Statement? Visit(PlusNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(MinusNode node, Statement param)
+    public Statement? Visit(MinusNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(TimesNode node, Statement param)
+    public Statement? Visit(TimesNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(FloatDivNode node, Statement param)
+    public Statement? Visit(FloatDivNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(IntDivNode node, Statement param)
+    public Statement? Visit(IntDivNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(ModulusNode node, Statement param)
+    public Statement? Visit(ModulusNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(ExponentiationNode node, Statement param)
+    public Statement? Visit(ExponentiationNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(LiteralNode node, Statement param)
+    public Statement? Visit(LiteralNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(VariableNode node, Statement param)
+    public Statement? Visit(VariableNode node, Statement? param)
     {
         return null;
     }
 
-    public Statement? Visit(AssignmentStmt node, Statement param)
-    {
-        _cfg.AddVertex(node);
-        _cfg.AddEdge(param, node);
-        return null;
-    }
-
-    public Statement? Visit(ReturnStmt node, Statement param)
+    public Statement? Visit(AssignmentStmt node, Statement? param)
     {
         _cfg.AddVertex(node);
-        _cfg.AddEdge(param, node);
+        if (param != null)
+        {
+            _cfg.AddEdge(param, node);
+        }
         return null;
     }
 
-    public Statement? Visit(BlockStmt node, Statement param)
+    public Statement? Visit(ReturnStmt node, Statement? param)
+    {
+        _cfg.AddVertex(node);
+        if (param != null)
+        {
+            _cfg.AddEdge(param, node);
+        }
+        return null;
+    }
+
+    public Statement? Visit(BlockStmt node, Statement? param)
     {
         if (_cfg.VertexCount() == 0) node.Statements[0].Accept(this, null);
         else node.Statements[0].Accept(this, last);
